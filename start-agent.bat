e792d22a1c445c4315c724d9ade0db7e4937fa51
Dano Onodera <danonodera.ex@gmail.com>
2026-05-23 21:09:38 +0900

    Add agent startup batch file for AGO PC

---
+++ start-agent.bat
@@ -0,0 +1,5 @@
+@echo off
+set BRAINTRUST_VPS_URL=http://210.131.215.16:5050
+set BRAINTRUST_TOKEN=4507171597d749daa3dd6d1d118122d3
+cd /d C:\Users\Administrator\Desktop\braintrust-api
+python agent_local.py
